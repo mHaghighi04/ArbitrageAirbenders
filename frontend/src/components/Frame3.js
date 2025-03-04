a9e42fafe5f5cefd70a81355f22ddbc7f8573895
John Vu <johnvu@169-231-83-29.wireless.ucsb.edu>
2025-03-04 15:34:26 -0800

    working version

--- frontend/src/components/Frame3.js
+++ frontend/src/components/Frame3.js
@@ -1,16 +1,8 @@
 // src/components/Frame3.js
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
-
-const sports = [
-  { id: "box1", name: "Football" },
-  { id: "box2", name: "Basketball" },
-  { id: "box3", name: "Baseball" },
-  { id: "box4", name: "Soccer" },
-  { id: "box5", name: "Tennis" },
-];
 
 function Frame3() {
   const location = useLocation();
@@ -42,16 +34,6 @@ function Frame3() {
     <div className="frame3" style={{marginTop: "-60px"}}>
       <div className="pickSportText">Pick a sport:</div>
 
-      {/* <div className="rectangle6"></div> */}
-
-      {/* Row 1 of boxes and labels */}
-      {/* <div id="boxf" className="box"></div>
-      <div id="textf" className="text">Fail</div>
-
-      <div id="boxnba" className="box"></div>
-      <div id="textnba" className="text">NBA</div> */}
-
-       {/* Example "Fail" option */}
        <div
         id="boxf"
         className="box"
@@ -67,7 +49,6 @@ function Frame3() {
         Fail
       </div>
 
-      {/* Example "NBA" option */}
       <div
         id="boxnba"
         className="box"
@@ -83,8 +64,6 @@ function Frame3() {
         NBA
       </div>
 
-
-      {/* Additional sport boxes with example labels */}
       <div
         id="box1"
         className="box"
@@ -157,54 +136,53 @@ function Frame3() {
         style={{ cursor: "pointer" }}
         onClick={() => handleSportClick("basketball_ncaab")}
       >
-        NCABB
+        NCAAB
       </div>
 
       {/* Row 2 */}
       <div id="box6" className="box"></div>
-      <div id="text6" className="text">Sport 6</div>
+      <div id="text6" className="text">Sport</div>
 
       <div id="box7" className="box"></div>
-      <div id="text7" className="text">Sport 7</div>
+      <div id="text7" className="text">Sport</div>
 
       <div id="box8" className="box"></div>
-      <div id="text8" className="text">Sport 8</div>
+      <div id="text8" className="text">Sport</div>
 
       <div id="box9" className="box"></div>
-      <div id="text9" className="text">Sport 9</div>
+      <div id="text9" className="text">Sport</div>
 
       <div id="box10" className="box"></div>
-      <div id="text10" className="text">Sport 10</div>
+      <div id="text10" className="text">Sport</div>
 
       <div id="box11" className="box"></div>
-      <div id="text11" className="text">Sport 11</div>
+      <div id="text11" className="text">Sport</div>
 
       <div id="box12" className="box"></div>
-      <div id="text12" className="text">Sport 12</div>
+      <div id="text12" className="text">Sport</div>
 
       {/* Row 3 */}
       <div id="box13" className="box"></div>
-      <div id="text13" className="text">Sport 13</div>
+      <div id="text13" className="text">Sport</div>
 
       <div id="box14" className="box"></div>
-      <div id="text14" className="text">Sport 14</div>
+      <div id="text14" className="text">Sport</div>
 
       <div id="box15" className="box"></div>
-      <div id="text15" className="text">Sport 15</div>
+      <div id="text15" className="text">Sport</div>
 
       <div id="box16" className="box"></div>
-      <div id="text16" className="text">Sport 16</div>
+      <div id="text16" className="text">Sport</div>
 
       <div id="box17" className="box"></div>
-      <div id="text17" className="text">Sport 17</div>
+      <div id="text17" className="text">Sport</div>
 
       <div id="box18" className="box"></div>
-      <div id="text18" className="text">Sport 18</div>
+      <div id="text18" className="text">Sport</div>
 
       <div id="box19" className="box"></div>
-      <div id="text19" className="text">Sport 19</div>
+      <div id="text19" className="text">Sport</div>
 
-      {/* Horizontal line */}
       <div className="line2"></div>
 
     </div>
